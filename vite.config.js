d2f473fe919b82384e5b3292ee381032766cb0da
Gargi <98906186+GargiMittal@users.noreply.github.com>
2026-07-19 18:56:59 +0530

    added automatic deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = '/Portfolio'
+
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/SwapnilPortfolio/',
+export default defineConfig(({ command }) => ({
+  base: command === 'serve' ? '/' : `${repoName}/`,
   plugins: [react()],
-})
+}))
